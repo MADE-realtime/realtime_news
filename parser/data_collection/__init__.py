dade9f54829f87bda225f2d668b369ea44cb3277
ermekaitygulov <ermek.imankulov@gmail.com>
2021-12-06 11:00:51 +0300

    parse tg and vk links; rename folder from url_files to source_files;

--- parser/data_collection/__init__.py
+++ parser/data_collection/__init__.py
@@ -2,6 +2,7 @@ from data_collection.news_spider import setup_ns_parser
 from data_collection.rss_spider import setup_rs_parser, setup_rs_db_parser
 from data_collection.yandex_spider import setup_ys_parser
 from data_collection.vk_spider import setup_vk_parser
+from data_collection.social_media_spider import setup_sm_parser
 
 
 SPIDER_PARSERS = {
@@ -11,4 +12,5 @@ SPIDER_PARSERS = {
     'yandex': ('Parse smi url from yandex news page', setup_ys_parser),
     'rss_db': ('Parse news from rss to db', setup_rs_db_parser),
     'vk': ('Parse news from vk', setup_vk_parser),
+    'social_media': ('Parse links to different social media', setup_sm_parser)
 }
--- parser/data_collection/tg_spider.py
+++ parser/data_collection/tg_spider.py
@@ -1,3 +1,15 @@
-api_id = '12465126'
+# from telethon import TelegramClient
+from telethon.sync import TelegramClient
+
+api_id = 12465126
 api_hash = '6f41fc346203d98715395048cff3e1eb'
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+
+entity = client.get_entity('https://t.me/tass_agency')
+print(entity.stringify()) #All paratmeters
+print(entity.id)
+msg = client.get_messages(entity.id, limit=10)
+print(msg[7].stringify())
 
