963b3d740571886986737f50421b314d6125e33b
ermekaitygulov <ermek.imankulov@gmail.com>
2021-12-06 11:00:51 +0300

    parse tg and vk links; rename folder from url_files to source_files;

--- parser/data_collection/__init__.py
+++ parser/data_collection/__init__.py
@@ -2,6 +2,7 @@ from data_collection.news_spider import setup_ns_parser
 from data_collection.rss_spider import setup_rs_parser, setup_rs_db_parser
 from data_collection.yandex_spider import setup_ys_parser
 from data_collection.vk_spider import setup_vk_parser, setup_vk_db_parser
+from data_collection.social_media_spider import setup_sm_parser
 
 
 SPIDER_PARSERS = {
@@ -12,4 +13,5 @@ SPIDER_PARSERS = {
     'rss_db': ('Parse news from rss to db', setup_rs_db_parser),
     'vk': ('Parse news from vk', setup_vk_parser),
     'vk_db': ('Parse news from vk to db', setup_vk_db_parser),
+    'social_media': ('Parse links to different social media', setup_sm_parser)
 }
---
+++ parser/data_collection/ok_spider.py
@@ -0,0 +1,7 @@
+"""
+Application ID: 512001308941.
+Публичный ключ приложения: COAKPIKGDIHBABABA.
+Секретный ключ приложения:  95C3FB547F430B544E82D448.
+Вечный session_key:tkn14YgWQ279xMzvjdfJtJuRajPvJtttKSCdawotwIt7ECm6L0PzFZLqwEpBQVe3xGYr7
+Session_secret_key:b2208fc58999b290093183f6fdfa6804
+"""
