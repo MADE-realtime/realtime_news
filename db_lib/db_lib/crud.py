360bb567e10ae74f295605dfb91c8b29d0de71c8
ermekaitygulov <ermek.imankulov@gmail.com>
2021-12-21 13:37:26 +0300

    add extract social network news and stat functions/methods to crud and DBNewsExtractor; add stopwords removing and lemmatizing to vktg clusterization

--- db_lib/db_lib/crud.py
+++ db_lib/db_lib/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
 
-from .models import News
+from .models import News, SocialNetworkNews, SocialNetworkStats
 from typing import List
 from datetime import date
 
@@ -65,6 +66,42 @@ def get_news_by_date(db: Session,
         .filter(News.time >= start_date) \
         .filter(News.time <= end_date) \
         .offset(skip).limit(limit).all()
+
+
+def get_social_network_news(db: Session,
+                            news_id: int) -> SocialNetworkNews:
+    return db.query(SocialNetworkNews) \
+        .filter(SocialNetworkNews.id == news_id).first()
+
+
+def get_social_network_stats(db: Session,
+                             post_id: int,
+                             social_network: str) -> SocialNetworkStats:
+    return db.query(SocialNetworkStats) \
+        .filter(SocialNetworkStats.post_id == post_id)\
+        .filter(SocialNetworkStats.social_network == social_network)\
+        .all()
+
+
+def get_social_network_stats_by_domain(db: Session,
+                                       domain: str) -> SocialNetworkStats:
+    join_expression = (SocialNetworkStats.post_id == SocialNetworkNews.post_id) & \
+                      (SocialNetworkStats.social_network == SocialNetworkNews.social_network)
+    query = [
+        func.max(SocialNetworkStats.comments).label('comments'),
+        func.max(SocialNetworkStats.reposts).label('reposts'),
+        func.max(SocialNetworkStats.views).label('views'),
+        func.max(SocialNetworkStats.likes).label('likes'),
+    ]
+    return db.query(query) \
+        .join(SocialNetworkNews, join_expression)\
+        .filter(SocialNetworkNews.source_name == domain)\
+        .group_by(
+            SocialNetworkStats.post_id,
+            SocialNetworkNews.date,
+            SocialNetworkNews.social_network
+        )\
+        .all()
 
 
 def create_news(db: Session, news: News):
--- web_service/src/handlers.py
+++ web_service/src/handlers.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 
 from config import FAVICON_PATH, TEMPLATE_NAME, SEARCH_TEMPLATE_NAME, SINGLE_TEMPLATE_NAME
 from datetime import datetime
@@ -6,13 +6,14 @@ from fastapi import Depends, FastAPI, Request, HTTPException
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from models import ListNews, News
+from models import ListNews
 from news_extractor import BaseNewsExtractor, DBNewsExtractor
 from sqlalchemy.orm import Session
 
 from db_lib.database import SessionLocal
-from utils import get_vs_plots_data, draw_by_day_plot
+from utils import get_vs_plots_data
 from session_logging import session_log
+from web_service.src.utils import get_vk_tg_stat_plot
 
 app = FastAPI()
 # NEWS_EXTRACTOR = PandasNewsExtractor(LENTA_MINI_DATASET_FILEPATH)
@@ -93,6 +94,7 @@ async def vs_search_handler(request: Request,
     response_class=HTMLResponse,
     # response_model=ListNews,
 )
+@session_log
 async def single_news_handler(
         request: Request, news_id: int, db: Session = Depends(get_db)
 ):
@@ -170,6 +172,39 @@ def get_topic_handler(
     return templates.TemplateResponse(
         TEMPLATE_NAME, {"request": request, 'news': news_list.news_list, 'stats': news_list.statistics}
     )
+
+
+@app.get(
+    '/vk_tg/{news_id}',
+    response_class=HTMLResponse,
+    # response_model=ListNews,
+)
+@session_log
+async def single_vk_tg_handler(
+        request: Request, news_id: int, db: Session = Depends(get_db)
+):
+    """
+    Get vk_tg news
+    :return:
+    """
+    news = NEWS_EXTRACTOR.show_vk_tg_news(db, news_id)['single_news']
+    post_id = news.post_id
+    social_network = news.social_network
+    news_stat = NEWS_EXTRACTOR.show_vk_tg_stat(db, post_id, social_network)
+    news_stat = get_vk_tg_stat_plot(news_stat)
+    if news:
+        return templates.TemplateResponse(
+            SINGLE_TEMPLATE_NAME, {
+                "request": request,
+                'single_news':  news,
+                'comments_plot': news_stat['comments'],
+                'likes_plot': news_stat['likes'],
+                'views_plot': news_stat['views'],
+                'reposts_plot': news_stat['reposts'],
+            }
+        )
+    else:
+        raise HTTPException(status_code=404, detail="No news with such id found")
 
 
 @app.get('/favicon.ico', response_class=FileResponse, include_in_schema=False)
