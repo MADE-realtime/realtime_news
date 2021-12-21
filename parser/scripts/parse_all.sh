c840b0a3b49f55bc3de84bf16ea3d39f405f4b6c
ermekaitygulov <ermek.imankulov@gmail.com>
2021-12-21 13:47:12 +0300

    fix parse scripts paths

--- parser/scripts/parse_all.sh
+++ parser/scripts/parse_all.sh
@@ -1,4 +1,4 @@
 cd ../
-/code/.venv/bin/python parse_news.py -d vk.json vk_db -start_urls source_files/vk.url
-/code/.venv/bin/python parse_news.py -d rss_news.json rss_db -s source_files/smi.url -r data_collection/rss_parser.yaml
-/code/.venv/bin/python parse_tg.py get_last -to_db
+/code/parser/.venv/bin/python parse_news.py -d vk.json vk_db -start_urls source_files/vk.url
+/code/parser/.venv/bin/python parse_news.py -d rss_news.json rss_db -s source_files/smi.url -r data_collection/rss_parser.yaml
+/code/parser/.venv/bin/python parse_tg.py get_last -to_db
